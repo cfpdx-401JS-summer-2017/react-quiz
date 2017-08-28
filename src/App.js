e41950550fd086f28d8afd1d6329b562752eca26
christylaguardia <christinelaguardia@gmail.com>
2017-08-28 14:13:37 -0700

    working on quiz

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CounterContainer from './counter.js';
 
 class App extends Component {
+
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //   };
+  // }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +19,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CounterContainer />
       </div>
     );
   }
