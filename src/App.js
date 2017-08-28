1416ac03327bf9797d125e607fef263f2395a39e
Joe Churchman <churchman.joe@gmail.com>
2017-08-28 14:15:36 -0700

    react quiz

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import CounterContainer from './CounterContainer.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <CounterContainer />
+    );
+  }
+}
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export function Counter(props) {
+    return <div>{props}</div>
+}
